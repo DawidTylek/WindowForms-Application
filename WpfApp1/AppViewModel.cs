f537fab01e4b834ecee92d7547b4da77865b273f
DawidTylek <dawidtylek.14@gmail.com>
2020-07-08 23:38:35 +0200

    First commit - Main app

---
+++ WpfApp1/AppViewModel.cs
@@ -0,0 +1,27 @@
+﻿using WpfApp1.ViewModels;
+
+namespace WpfApp1
+{
+    public class AppViewModel : MartialArtsApp2
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { OnPropertyChanged(ref _currentView, value); }
+        }
+ 
+        private MaArtViewModel _MartialVM;
+        public MaArtViewModel MartialVM
+        {
+            get { return _MartialVM; }
+            set { OnPropertyChanged(ref _MartialVM, value); }
+        }
+ 
+        public AppViewModel()
+        {
+            MartialVM = new MaArtViewModel();
+            CurrentView = MartialVM;
+        }
+    }
+}
