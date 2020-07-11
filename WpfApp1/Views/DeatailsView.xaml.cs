dbdb331bd3604611b025ec1798ad346d7241786a
DawidTylek <dawidtylek.14@gmail.com>
2020-07-11 23:08:23 +0200

    Control buttons and information view

---
+++ WpfApp1/Views/DeatailsView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp1.Views
+{
+    public partial class DeatailsView : UserControl
+    {
+        public DeatailsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
