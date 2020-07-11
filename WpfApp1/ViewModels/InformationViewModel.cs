ce0e21a7cae53bae8d7abc85d884445b54d0a002
DawidTylek <dawidtylek.14@gmail.com>
2020-07-12 00:04:46 +0200

    Simple edit mode

--- WpfApp1/ViewModels/InformationViewModel.cs
+++ WpfApp1/ViewModels/InformationViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using WpfApp1.Models;
 
 namespace WpfApp1.ViewModels
@@ -13,12 +14,41 @@ namespace WpfApp1.ViewModels
             get { return _selectedSchools; }
             set { OnPropertyChanged(ref _selectedSchools, value); }
         }
-        
+
+        private bool _isEditMode;
+        public bool IsEditMode
+        {
+            get { return _isEditMode; }
+            set { OnPropertyChanged(ref _isEditMode, value);
+                OnPropertyChanged("IsDisplayMode");
+            }
+        }
+
+        public bool IsDisplayMode
+        {
+            get { return !_isEditMode;  }
+        }
+
         public ObservableCollection<Schools> Schools{ get; private set; }
 
+        public ICommand EditCommand { get; private set; }
+        
         public InformationViewModel()
         {
-            
+            EditCommand = new MartialArtsApp(Edit, CanEdit);
+        }
+
+        private bool CanEdit()
+        {
+            if (SelectedSchools == null)
+                return false;
+
+            return !IsEditMode;
+        }
+
+        private void Edit()
+        {
+            IsEditMode = true;
         }
 
         public void LoadSchools(IEnumerable<Schools> schools)
